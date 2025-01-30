fae41ee318500b159ff6efd049c4797ab10cc975
JesCarlosMV <amgtk@hotmail.com>
2025-01-30 13:50:38 +0100

    firstcommit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+//! ARCHIVO QUE GUARDA LAS FUENTES DE TODA LA APP
+
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
